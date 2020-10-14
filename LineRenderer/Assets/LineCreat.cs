d68d6a2d9eab19a97c489c944a63308934f90edf
sukrubeyy <sukru.beyy@outlook.com>
2020-10-14 22:56:41 +0300

    Great :D

---
+++ LineRenderer/Assets/LineCreat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreat : MonoBehaviour
+{
+    public GameObject prefabLine;
+    lineRendererScript lineScript;
+    Vector2 mousePos;
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject lineActive = Instantiate(prefabLine);
+            lineScript = lineActive.GetComponent<lineRendererScript>();
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            lineScript = null;
+        }       
+        if(lineScript != null)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            lineScript.drawLine(mousePos);
+        }
+    }
+}
---
+++ LineRenderer/Assets/lineRendererScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+//Şükrü Çay 
+public class lineRendererScript : MonoBehaviour
+{
+    public  LineRenderer lineRD;
+    public EdgeCollider2D edgeCollider;
+
+    List<Vector2> points;
+
+    public void drawLine(Vector2 mousePosition)
+    {
+        // Eğer iki nokta arası yok ise vector2 değeri x=0,y=0 döndürüyor. 
+        // Daha sonra newPoint methoduna yönlendiriliyor.
+        if(points==null)
+        {
+            points = new List<Vector2>();
+            newPoint(mousePosition);
+            return;
+        }
+        //Point1 ve Point2 arasındaki mesafe .1f 'den büyük ise pointleri al ve newPoint methoduna point değerini parametre olarak gönderiyoruz.
+        if(Vector2.Distance(points.Last(),mousePosition)>.1f)
+        {
+            newPoint(mousePosition);
+        }
+    }
+    void newPoint(Vector2 point)
+    {
+        points.Add(point);
+       // lineRD.positionCount = points.Count;
+        lineRD.numPositions = points.Count;
+        lineRD.SetPosition(points.Count - 1, point);
+        if(points.Count>1)
+        {
+            edgeCollider.points = points.ToArray();
+        }
+        
+    }
+}
